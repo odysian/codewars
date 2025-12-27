7344b5e8c996d53c886aba347e9b41dfe7755da1
odysian <odyssian7@gmail.com>
2025-12-27 15:23:54 -0500

    rename coding meetup, add kata #2

--- 7-kyu/coding-meetup.py
+++ 7-kyu/coding-meetup.py
@@ -1,3 +1,4 @@
+# CODING MEETUP 1
 def count_developers(lst):
     # Sum 1 for every developer where continent is Europe AND language is JS
     return sum(
@@ -5,3 +6,14 @@ def count_developers(lst):
         for dev in lst
         if dev["continent"] == "Europe" and dev["language"] == "JavaScript"
     )
+
+
+# CODING MEETUP 2
+def greet_developers(lst):
+
+    for dev in lst:
+        dev["greeting"] = (
+            f"Hi {dev['firstName']}, what do you like the most about {dev['language']}?"
+        )
+
+    return lst
